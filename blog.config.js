27653fc8296f4f47890e8035d1c9d848499bc17f
Zhangt <zt345564023@gmail.com>
2023-02-07 16:03:19 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,8 +25,7 @@ const BLOG = {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '', // Remove the value or replace it with your own google site verification code
   },
-  notionPageId:
-    process.env.NOTION_PAGE_ID || 'eb2ef9da57c54217bbb6888d20e735d0', // DO NOT CHANGE THIS！！！
+  notionPageId: 'eb2ef9da57c54217bbb6888d20e735d0', // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
     provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
